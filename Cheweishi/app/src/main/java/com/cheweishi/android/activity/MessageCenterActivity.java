ff6266604072fbcbada1956aed7433f498e0bae2
Tanck <softtanck@163.com>
2016-03-21 16:44:43 +0800

    增加消息中心返回键退出.

--- Cheweishi/app/src/main/java/com/cheweishi/android/activity/MessageCenterActivity.java
+++ Cheweishi/app/src/main/java/com/cheweishi/android/activity/MessageCenterActivity.java
@@ -2,19 +2,30 @@ package com.cheweishi.android.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.cheweishi.android.cheweishi.R;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
 
 /**
  * Created by tangce on 3/21/2016.
  */
 
+
+
 public class MessageCenterActivity extends BaseActivity implements View.OnClickListener {
+
+
+    @ViewInject(R.id.left_action)
+    private Button left_action;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_center_new);
+        ViewUtils.inject(this);
     }
 
     @OnClick(R.id.left_action)
