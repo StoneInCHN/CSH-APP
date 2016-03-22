1577bad944163efa0dd145850e8b19e21997431a
Tanck <softtanck@163.com>
2016-03-22 17:22:29 +0800

    更新商场界面.

--- Cheweishi/app/src/main/java/com/cheweishi/android/activity/CarShopActivity.java
+++ Cheweishi/app/src/main/java/com/cheweishi/android/activity/CarShopActivity.java
@@ -2,12 +2,25 @@ package com.cheweishi.android.activity;
 
 import android.os.Bundle;
 
+import com.cheweishi.android.adapter.CarShopAdapter;
+import com.cheweishi.android.cheweishi.R;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
 /**
  * Created by Administrator on 2016/3/21.
  */
 public class CarShopActivity extends BaseActivity {
+
+    private CarShopAdapter adapter;
+
+    private PullToRefreshListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car_shop);
+        listView = (PullToRefreshListView) findViewById(R.id.pl_car_shop);
+        adapter = new CarShopAdapter();
+        listView.setAdapter(adapter);
     }
 }
---
+++ Cheweishi/app/src/main/java/com/cheweishi/android/adapter/CarShopAdapter.java
@@ -0,0 +1,35 @@
+package com.cheweishi.android.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by tangce on 3/22/2016.
+ */
+public class CarShopAdapter extends BaseAdapter {
+
+    public CarShopAdapter(){
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
