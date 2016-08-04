bffb6b82abfad4d24cd853c701d73b52ec9c91b1
Tanck <softanck@163.com>
2016-08-04 16:17:09 +0800

    组织代码.

--- Cheweishi/app/src/main/java/com/cheweishi/android/utils/mapUtils/MapSearchUtil.java
+++ Cheweishi/app/src/main/java/com/cheweishi/android/utils/mapUtils/MapSearchUtil.java
@@ -24,208 +24,204 @@ import com.baidu.mapapi.search.sug.SuggestionSearchOption;
 import com.cheweishi.android.utils.StringUtil;
 
 public class MapSearchUtil {
-	/**
-	 * 路线规划-开车
-	 */
-	public static final int ROUTEPLAN_DRIVE = 1001;
-	/**
-	 * 路线规划-步行
-	 */
-	public static final int ROUTEPLAN_WALK = 1002;
-	/**
-	 * 路线规划-公交
-	 */
-	public static final int ROUTEPLAN_TRANSIT = 1003;
-	/**
-	 * 分享链接-位置
-	 */
-	public static final int SHAREURL_LOCATION = 1004;
-	/**
-	 * 分享链接-兴趣点详情
-	 */
-	public static final int SHAREURL_POIDETAIL = 1005;
+    /**
+     * 路线规划-开车
+     */
+    public static final int ROUTEPLAN_DRIVE = 1001;
+    /**
+     * 路线规划-步行
+     */
+    public static final int ROUTEPLAN_WALK = 1002;
+    /**
+     * 路线规划-公交
+     */
+    public static final int ROUTEPLAN_TRANSIT = 1003;
+    /**
+     * 分享链接-位置
+     */
+    public static final int SHAREURL_LOCATION = 1004;
+    /**
+     * 分享链接-兴趣点详情
+     */
+    public static final int SHAREURL_POIDETAIL = 1005;
 
-	/* 路线规划 */
-	private RoutePlanSearch routePlanSearch;
+    /* 路线规划 */
+    private RoutePlanSearch routePlanSearch;
 
-	/* 地理位置编译 */
-	private GeoCoder geoCoder;
+    /* 地理位置编译 */
+    private GeoCoder geoCoder;
 
-	/* 兴趣点搜索 */
-	private PoiSearch poiSearch;
+    /* 兴趣点搜索 */
+    private PoiSearch poiSearch;
 
-	/* 建议搜索 */
-	private SuggestionSearch suggestionSearch;
+    /* 建议搜索 */
+    private SuggestionSearch suggestionSearch;
 
-	/* 链接分享搜索 */
-	private ShareUrlSearch shareUrlSearch;
+    /* 链接分享搜索 */
+    private ShareUrlSearch shareUrlSearch;
 
-	/**
-	 * 路线规划
-	 * 
-	 * @param planStyle
-	 *            规划方式
-	 * @param sLatLon
-	 * @param eLatLon
-	 * @param onGetRoutePlanResultListener
-	 */
-	public void startRoutePlan(int planStyle, LatLng sLatLon, LatLng eLatLon,
-			OnGetRoutePlanResultListener onGetRoutePlanResultListener) {
-		if (routePlanSearch == null) {
-			routePlanSearch = RoutePlanSearch.newInstance();
-		}
-		routePlanSearch
-				.setOnGetRoutePlanResultListener(onGetRoutePlanResultListener);
-		PlanNode sNode = PlanNode.withLocation(sLatLon);
-		PlanNode eNode = PlanNode.withLocation(eLatLon);
+    /**
+     * 路线规划
+     *
+     * @param planStyle                    规划方式
+     * @param sLatLon
+     * @param eLatLon
+     * @param onGetRoutePlanResultListener
+     */
+    public void startRoutePlan(int planStyle, LatLng sLatLon, LatLng eLatLon,
+                               OnGetRoutePlanResultListener onGetRoutePlanResultListener) {
+        if (routePlanSearch == null) {
+            routePlanSearch = RoutePlanSearch.newInstance();
+        }
+        routePlanSearch
+                .setOnGetRoutePlanResultListener(onGetRoutePlanResultListener);
+        PlanNode sNode = PlanNode.withLocation(sLatLon);
+        PlanNode eNode = PlanNode.withLocation(eLatLon);
 
-		switch (planStyle) {
-		case ROUTEPLAN_TRANSIT:
-			routePlanSearch.transitSearch(new TransitRoutePlanOption().from(
-					sNode).to(eNode));
-			break;
-		case ROUTEPLAN_DRIVE:
-			routePlanSearch.drivingSearch(new DrivingRoutePlanOption().from(
-					sNode).to(eNode));
-			break;
-		case ROUTEPLAN_WALK:
-			routePlanSearch.walkingSearch(new WalkingRoutePlanOption().from(
-					sNode).to(eNode));
-			break;
+        switch (planStyle) {
+            case ROUTEPLAN_TRANSIT:
+                routePlanSearch.transitSearch(new TransitRoutePlanOption().from(
+                        sNode).to(eNode));
+                break;
+            case ROUTEPLAN_DRIVE:
+                routePlanSearch.drivingSearch(new DrivingRoutePlanOption().from(
+                        sNode).to(eNode));
+                break;
+            case ROUTEPLAN_WALK:
+                routePlanSearch.walkingSearch(new WalkingRoutePlanOption().from(
+                        sNode).to(eNode));
+                break;
 
-		default:
-		}
+            default:
+        }
 
-	}
+    }
 
-	/**
-	 * 开始geocode编码
-	 * 
-	 * @param city
-	 * @param address
-	 * @param onGetGeoCoderResultListener
-	 */
-	public void startGeoCode(String city, String address,
-			OnGetGeoCoderResultListener onGetGeoCoderResultListener) {
-		initGeoCoder(onGetGeoCoderResultListener);
+    /**
+     * 开始geocode编码
+     *
+     * @param city
+     * @param address
+     * @param onGetGeoCoderResultListener
+     */
+    public void startGeoCode(String city, String address,
+                             OnGetGeoCoderResultListener onGetGeoCoderResultListener) {
+        initGeoCoder(onGetGeoCoderResultListener);
 
-		geoCoder.geocode(new GeoCodeOption().city(city).address(address));
-	}
+        geoCoder.geocode(new GeoCodeOption().city(city).address(address));
+    }
 
-	/**
-	 * 开始geocoder反编译
-	 * 
-	 * @param latLng
-	 * @param onGetGeoCoderResultListener
-	 */
-	public void startReverseGeoCode(LatLng latLng,
-			OnGetGeoCoderResultListener onGetGeoCoderResultListener) {
-		initGeoCoder(onGetGeoCoderResultListener);
+    /**
+     * 开始geocoder反编译
+     *
+     * @param latLng
+     * @param onGetGeoCoderResultListener
+     */
+    public void startReverseGeoCode(LatLng latLng, OnGetGeoCoderResultListener onGetGeoCoderResultListener) {
+        initGeoCoder(onGetGeoCoderResultListener);
 
-		geoCoder.reverseGeoCode(new ReverseGeoCodeOption().location(latLng));
-	}
+        geoCoder.reverseGeoCode(new ReverseGeoCodeOption().location(latLng));
+    }
 
-	/**
-	 * 初始化geocoder并设置监听
-	 * 
-	 * @param onGetGeoCoderResultListener
-	 */
-	private void initGeoCoder(
-			OnGetGeoCoderResultListener onGetGeoCoderResultListener) {
-		if (geoCoder == null) {
-			geoCoder = GeoCoder.newInstance();
-			geoCoder.setOnGetGeoCodeResultListener(onGetGeoCoderResultListener);
-		}
-	}
+    /**
+     * 初始化geocoder并设置监听
+     *
+     * @param onGetGeoCoderResultListener
+     */
+    private void initGeoCoder(OnGetGeoCoderResultListener onGetGeoCoderResultListener) {
+        if (geoCoder == null) {
+            geoCoder = GeoCoder.newInstance();
+            geoCoder.setOnGetGeoCodeResultListener(onGetGeoCoderResultListener);
+        }
+    }
 
-	/**
-	 * 简单poi搜索
-	 * 
-	 * @param city
-	 * @param onGetPoiSearchResultListener
-	 */
-	public void startPoiSearch(String city,
-			OnGetPoiSearchResultListener onGetPoiSearchResultListener) {
-		if (poiSearch == null) {
-			poiSearch = PoiSearch.newInstance();
-			poiSearch
-					.setOnGetPoiSearchResultListener(onGetPoiSearchResultListener);
-		}
+    /**
+     * 简单poi搜索
+     *
+     * @param city
+     * @param onGetPoiSearchResultListener
+     */
+    public void startPoiSearch(String city,
+                               OnGetPoiSearchResultListener onGetPoiSearchResultListener) {
+        if (poiSearch == null) {
+            poiSearch = PoiSearch.newInstance();
+            poiSearch
+                    .setOnGetPoiSearchResultListener(onGetPoiSearchResultListener);
+        }
 
-		poiSearch
-				.searchInCity(new PoiCitySearchOption().city(city).keyword(""));
+        poiSearch
+                .searchInCity(new PoiCitySearchOption().city(city).keyword(""));
 
-	}
+    }
 
-	/**
-	 * 建议点搜索
-	 * 
-	 * @param city
-	 * @param keyWord
-	 * @param onGetSuggestionResultListener
-	 */
-	public void startSuggetSearch(String city, String keyWord,
-			OnGetSuggestionResultListener onGetSuggestionResultListener) {
-		if (suggestionSearch == null) {
-			suggestionSearch = SuggestionSearch.newInstance();
-			suggestionSearch
-					.setOnGetSuggestionResultListener(onGetSuggestionResultListener);
-		}
-		if (!StringUtil.isEmpty(keyWord) && !StringUtil.isEmpty(city)) {
-			suggestionSearch.requestSuggestion(new SuggestionSearchOption()
-				.keyword(keyWord).city(city));
-		}
+    /**
+     * 建议点搜索
+     *
+     * @param city
+     * @param keyWord
+     * @param onGetSuggestionResultListener
+     */
+    public void startSuggetSearch(String city, String keyWord,
+                                  OnGetSuggestionResultListener onGetSuggestionResultListener) {
+        if (suggestionSearch == null) {
+            suggestionSearch = SuggestionSearch.newInstance();
+            suggestionSearch
+                    .setOnGetSuggestionResultListener(onGetSuggestionResultListener);
+        }
+        if (!StringUtil.isEmpty(keyWord) && !StringUtil.isEmpty(city)) {
+            suggestionSearch.requestSuggestion(new SuggestionSearchOption()
+                    .keyword(keyWord).city(city));
+        }
 
-	}
+    }
 
-	/**
-	 * 
-	 * @param type
-	 * @param latLng
-	 * @param poiUid
-	 * @param onGetShareUrlResultListener
-	 */
-	public void startShareUrlSearch(int type, LatLng latLng, String poiUid,
-			OnGetShareUrlResultListener onGetShareUrlResultListener) {
-		if (shareUrlSearch == null) {
-			shareUrlSearch = ShareUrlSearch.newInstance();
-			shareUrlSearch
-					.setOnGetShareUrlResultListener(onGetShareUrlResultListener);
-		}
-		if (type == SHAREURL_LOCATION) {
-			shareUrlSearch.requestLocationShareUrl(new LocationShareURLOption()
-					.location(latLng));
-			return;
-		}
-		shareUrlSearch.requestPoiDetailShareUrl(new PoiDetailShareURLOption()
-				.poiUid(poiUid));
-	}
+    /**
+     * @param type
+     * @param latLng
+     * @param poiUid
+     * @param onGetShareUrlResultListener
+     */
+    public void startShareUrlSearch(int type, LatLng latLng, String poiUid,
+                                    OnGetShareUrlResultListener onGetShareUrlResultListener) {
+        if (shareUrlSearch == null) {
+            shareUrlSearch = ShareUrlSearch.newInstance();
+            shareUrlSearch
+                    .setOnGetShareUrlResultListener(onGetShareUrlResultListener);
+        }
+        if (type == SHAREURL_LOCATION) {
+            shareUrlSearch.requestLocationShareUrl(new LocationShareURLOption()
+                    .location(latLng));
+            return;
+        }
+        shareUrlSearch.requestPoiDetailShareUrl(new PoiDetailShareURLOption()
+                .poiUid(poiUid));
+    }
 
-	/**
-	 * 释放变量
-	 */
-	public void onDestory() {
-		if (routePlanSearch != null) {
-			routePlanSearch.destroy();
-			routePlanSearch = null;
-		}
+    /**
+     * 释放变量
+     */
+    public void onDestory() {
+        if (routePlanSearch != null) {
+            routePlanSearch.destroy();
+            routePlanSearch = null;
+        }
 
-		if (geoCoder != null) {
-			geoCoder.destroy();
-			geoCoder = null;
-		}
-		if (poiSearch != null) {
-			poiSearch.destroy();
-			poiSearch = null;
-		}
+        if (geoCoder != null) {
+            geoCoder.destroy();
+            geoCoder = null;
+        }
+        if (poiSearch != null) {
+            poiSearch.destroy();
+            poiSearch = null;
+        }
 
-		if (suggestionSearch != null) {
-			suggestionSearch.destroy();
-			suggestionSearch = null;
-		}
-		if (shareUrlSearch != null) {
-			shareUrlSearch.destroy();
-			shareUrlSearch = null;
-		}
-	}
+        if (suggestionSearch != null) {
+            suggestionSearch.destroy();
+            suggestionSearch = null;
+        }
+        if (shareUrlSearch != null) {
+            shareUrlSearch.destroy();
+            shareUrlSearch = null;
+        }
+    }
 }
