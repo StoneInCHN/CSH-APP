d4efe8efe7e99f53654faf55fc653787f477c445
Tanck <softanck@163.com>
2016-09-01 18:27:44 +0800

    完善增加收货地址界面.

--- Cheweishi/app/src/main/java/com/cheweishi/android/activity/AddressManagerActivity.java
+++ Cheweishi/app/src/main/java/com/cheweishi/android/activity/AddressManagerActivity.java
@@ -1,13 +1,17 @@
 package com.cheweishi.android.activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.cheweishi.android.R;
 import com.cheweishi.android.entity.MyRecevieAddressResponse;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
 
 /**
  * Created by tangce on 8/31/2016.
@@ -19,6 +23,21 @@ public class AddressManagerActivity extends BaseActivity {
 
     @ViewInject(R.id.title)
     private TextView title; // 标题
+
+    @ViewInject(R.id.et_adm_name)
+    private EditText et_adm_name;//收货人名字
+
+    @ViewInject(R.id.et_adm_phone)
+    private EditText et_adm_phone;//收货人电话
+
+    @ViewInject(R.id.tv_adm_area)
+    private TextView tv_adm_area;//收货人地区
+
+    @ViewInject(R.id.cb_adm_def)
+    private CheckBox cb_adm_def;//是否默认
+
+    @ViewInject(R.id.et_adm_detail_address)
+    private EditText et_adm_detail_address;//收货人详细地址
 
     private MyRecevieAddressResponse.MsgBean data;
 
@@ -36,8 +55,25 @@ public class AddressManagerActivity extends BaseActivity {
         data = (MyRecevieAddressResponse.MsgBean) getIntent().getSerializableExtra("data");
         if (null != data) {
             title.setText(R.string.edit_address);
+            et_adm_name.setText(data.getConsignee());
+            et_adm_phone.setText(data.getPhone());
+            et_adm_detail_address.setText(data.getAddress());
+            tv_adm_area.setText(data.getAreaName());
+            if (data.isIsDefault())
+                cb_adm_def.setChecked(true);
         } else {
             title.setText(R.string.add_new_address);
+        }
+    }
+
+    @OnClick({R.id.left_action, R.id.tv_adm_area})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.left_action:
+                finish();
+                break;
+            case R.id.tv_adm_area: // 地区修改
+                break;
         }
     }
 }
